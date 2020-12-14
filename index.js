258b63a757526c136c32232ac84a51dc3a2979fc
jeichlcof <74484217+jeichlcof@users.noreply.github.com>
2020-12-14 21:16:39 +0900

    12-14

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
-let mainImage = document.querySelector("container__main__img");
+const mainImage = document.querySelector(".container__main__img");
 var backgroundImg = new Array(
   "image/1.jpg",
   "image/2.jpg",
   "image/3.jpg",
   "image/4.jpg"
 );
-let i = 0;
+let imageNumber = 1;
+
+function nextNumber() {
+  imageNumber = imageNumber + 1;
+  if (imageNumber === 5) {
+    imageNumber = 4;
+  }
+  return imageNumber;
+}
+
+function prevNumber() {
+  imageNumber = imageNumber - 1;
+  if (imageNumber === 0) {
+    imageNumber = 1;
+  }
+  return imageNumber;
+}
+
+function paintImage(imgNumber) {
+  const image = new Image();
+  image.src = `image/${imgNumber}.jpg`;
+  mainImage.removeChild(mainImage.firstChild);
+  image.classList.add("bgImage");
+  mainImage.prepend(image);
+}
+
 next.onclick = function () {
-  mainImage.style.backgroundImage = `url("` + backgroundImg[i + 1] + `")`;
+  const getNumber = nextNumber();
+  paintImage(getNumber);
 };
+prev.onclick = function () {
+  const getNumber = prevNumber();
+  paintImage(getNumber);
+};
+
+function init() {
+  const getNumber = prevNumber();
+  paintImage(getNumber);
+}
+init();
